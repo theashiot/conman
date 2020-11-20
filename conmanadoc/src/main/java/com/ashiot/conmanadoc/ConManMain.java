efed0af91936c7c64380c29fb1ce4fc5db14398a
ashiot <amehenda@redhat.com>
2020-11-20 15:05:20 +0530

    Create the maven project

---
+++ conmanadoc/src/main/java/com/ashiot/conmanadoc/ConManMain.java
@@ -0,0 +1,13 @@
+package com.ashiot.conmanadoc;
+
+/**
+ * Hello world!
+ *
+ */
+public class ConManMain 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
