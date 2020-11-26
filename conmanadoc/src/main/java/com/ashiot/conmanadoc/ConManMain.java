1c9a96a6eb202ab0fa1795f807a64b5c1522f849
ashiot <amehenda@redhat.com>
2020-11-26 12:33:26 +0530

    safety

--- conmanadoc/src/main/java/com/ashiot/conmanadoc/ConManMain.java
+++ conmanadoc/src/main/java/com/ashiot/conmanadoc/ConManMain.java
@@ -1,5 +1,6 @@
 package com.ashiot.conmanadoc;
 
+import com.ashiot.conmanadoc.Content;
 /**
  * Hello world!
  *
@@ -9,5 +10,42 @@ public class ConManMain
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+		int run =  0;
+		Content content = new Content();
+		if (args.length == 0)
+		{
+			System.out.println ("Incorrect input");
+			System.out.println ("Usage:\n<command> <type> \"Module name\"");
+			System.out.println ("type is one of:\ncon\t concept module\nproc\tprocedure module\nref\treference moule");
+		}
+		else {
+				
+		switch (args[0])
+		{
+			case "con" : 
+				content.initializeContent("con", args[1]);
+				break;
+			case "proc" : 
+				content.initializeContent("proc", args[1]);
+				break;
+			case "ref" : 
+				content.initializeContent("ref", args[1]);
+				break;
+			case "help":
+				System.out.println ("Usage:\n<command> <type> \"Module name\"");
+				System.out.println ("type is one of:\ncon\t concept module\nproc\tprocedure module\nref\treference moule");
+				break;
+			default:
+				System.out.println ("Incorrect input");
+				System.out.println ("Usage:\n<command> <type> \"Module name\"");
+				System.out.println ("type is one of:\ncon\t concept module\nproc\tprocedure module\nref\treference moule");
+		}
+		//System.out.println (args[0]);
+		//System.out.println (args[1]);
+		/*
+
+		content.initializeContent("proc", "My amazing module.java name 1");
+*/
+		}
     }
 }
