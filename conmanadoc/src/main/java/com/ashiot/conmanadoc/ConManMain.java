b37840f469765033ccbcc7b7a978ca17cd75d3e6
ashiot <amehenda@redhat.com>
2021-01-15 01:43:48 +0530

    Refactor Menu and ignore comments by default

--- conmanadoc/src/main/java/com/ashiot/conmanadoc/ConManMain.java
+++ conmanadoc/src/main/java/com/ashiot/conmanadoc/ConManMain.java
@@ -1,62 +1,36 @@
 package com.ashiot.conmanadoc;
 
 import com.ashiot.conmanadoc.Content;
+import com.ashiot.conmanadoc.InputParser;
+
+import java.util.HashMap;
+
 import com.ashiot.conmanadoc.Assemble;
 
 public class ConManMain 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-		int run =  0;
-		Content content = new Content();
-		Assemble assembly = new Assemble();
-		String context;
-		if (args.length == 0)
-		{
-			System.out.println ("Incorrect input");
-			System.out.println ("Usage:\n<command> <type> \"Module name\"");
-			System.out.println ("type is one of:\ncon\t concept module\nproc\tprocedure module\nref\treference moule");
-			System.out.println ("OR <command> assembly \"Assembly name\" \"context\" (context is to define the assembly level context");
-		}
-		else {
-				
-		switch (args[0])
-		{
-			case "con" : 
-				content.initializeContent("con", args[1]);
-				break;
-			case "proc" : 
-				content.initializeContent("proc", args[1]);
-				break;
-			case "ref" : 
-				content.initializeContent("ref", args[1]);
-				break;
-			case "assembly" :
-				if (args.length == 2)
-					context = args[1];
-				else
-					context = args[2];
-				
-				assembly.initializeAssembly(args[1], context);
-				break;
-			case "help":
-				System.out.println ("Usage:\n<command> <type> \"Module name\"");
-				System.out.println ("type is one of:\ncon\t concept module\nproc\tprocedure module\nref\treference module");
-				System.out.println ("\nOR <command> assembly \"Assembly name\" \"context\" (context is to define the assembly level context");
-				break;
-			default:
-				System.out.println ("Incorrect input");
-				System.out.println ("Usage:\n<command> <type> \"Module name\"");
-				System.out.println ("type is one of:\ncon\t concept module\nproc\tprocedure module\nref\treference module");
-				System.out.println ("OR <command> assembly \"Assembly name\" \"context\" (context is to define the assembly level context");
-		}
-		//System.out.println (args[0]);
-		//System.out.println (args[1]);
-		/*
+    	Content content = new Content();
+    	Assemble assembly = new Assemble();
+    	HashMap <String,String> commandMap;
+    	InputParser input = new InputParser(args);
+    	commandMap = input.getCommands();
+    	if (commandMap != null && (input.help == false))
+    	{
+    		
+    		if (commandMap.get("--type").equals("assembly")) {
+    			if (commandMap.get("--context") == null) {
+    				assembly.initializeAssembly(commandMap.get("--name"),commandMap.get("--name"));
+    			}
+    			else {
+    				assembly.initializeAssembly(commandMap.get("--name"),commandMap.get("--context"));
 
-		content.initializeContent("proc", "My amazing module.java name 1");
-*/
-		}
+    			}
+    		}
+    		else
+    			content.initializeContent(commandMap.get("--type"), commandMap.get("--name"));
+    	}
+
     }
 }
---
+++ conmanadoc/src/main/java/com/ashiot/conmanadoc/InputParser.java
@@ -0,0 +1,171 @@
+package com.ashiot.conmanadoc;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class InputParser {
+	
+	private ArrayList <String> commandArgs;
+	private final String contentTypeArg = "--type";
+	private final String contentNameArg = "--name";
+	private final String contentContextArg = "--context";
+	private final String contentCommentArg = "--comments";
+	private final String helpArg = "--help";
+	public boolean help;
+	private HashMap<String,String> commands;
+
+	private enum contentType {con,proc,ref,assembly};
+	private boolean commandValid;
+	
+	public InputParser (String args[]) {
+		commandArgs = new ArrayList <String> ();
+		commandArgs.add(contentTypeArg);
+		commandArgs.add(contentNameArg);
+	//	commandArgs.add(contentContextArg);
+	//	commandArgs.add(contentCommentArg);
+		this.commandValid = false;
+		this.help = false;
+		this.commands = new HashMap <String,String>();
+		
+		for (String item : commandArgs) {
+			commands.put(item, null);
+		}
+		
+		if (args.length == 0)
+			this.printUsage();
+		
+		else {
+			this.createHashMapFromInputArgs (args);
+			this.validateCommand();
+		}
+	}
+	
+	private boolean validateContentType (String content) {
+		contentType[] values = contentType.values();
+		
+		for (contentType value : values) {
+			if (value.name().equals(content)) {
+				//System.out.println ("Correct type");
+				this.commandValid = true;
+				return true;
+			}
+		}
+		this.commandValid = false;
+		return false;
+	}
+	
+	public HashMap <String,String> getCommands () {
+		if (this.commandValid)
+			return this.commands;
+		
+		else
+			return null;
+	}
+	
+	private boolean validateCommand () {
+		
+		if (commands.containsKey(helpArg))
+		{
+			System.out.println ("in help");
+			this.commandValid = true;
+			this.printUsage();
+			this.help = true;
+			return true;
+		}
+
+		else {
+			for (String hash : commands.keySet())
+			{
+				if (commands.get(hash) == null) {
+					this.commandValid = false;
+					System.out.println("incorrect input");
+					return false;
+				}
+			}
+			this.commandValid = true;
+		}
+		
+		return true;
+	}
+	
+	private void printUsage () {
+		String usageLine = "\nUsage:\n\njava -jar conman.jar " +  contentTypeArg + " <TYPE> " + contentNameArg + " <NAME> \n";
+		String optionsLine = "TYPE is one of:\ncon\t\t:\tuse for concept module\nproc\t\t:\tuse for procedure module\nref\t\t:\tuse for reference module\nassembly\t:\tuse for assembly\n";
+		String exampleLine = "Example: java -jar conman "+contentTypeArg+" con "+contentNameArg+" \"My new module\"\n";		
+		String optionalContextLine = "Optionally use "+contentContextArg+" with assembly to provide a custom assembly context\n";
+		String assemblyExampleLine = "Example: java -jar conman \""+contentTypeArg+" assembly "+contentNameArg+" \"My new assembly\" "+contentContextArg+" \"my custom context\"\n";
+		System.out.println (usageLine);
+		System.out.println(optionsLine);
+		System.out.println (exampleLine);
+		System.out.println (optionalContextLine);
+		System.out.println (assemblyExampleLine);
+	}
+	
+	private void createHashMapFromInputArgs (String args[]) {
+		int index = 0;	
+		
+		
+		for (String arg : args) {
+			//System.out.println ("index = " + index + " " + arg);
+			
+			if (arg.equals(helpArg)) {
+				commands.put(helpArg, null);
+				return;
+			}
+			else if (arg.equals(contentTypeArg)) {
+				if ((index+1) < (args.length)) {
+					if (args[index + 1].startsWith("--")) {
+						System.out.println("incorrect input");
+						break;
+					}
+					else {
+						if (validateContentType(args[index + 1]))
+							commands.put(contentTypeArg, args[index+1]);
+						
+						else {
+							System.out.println("incorrect input");
+							break;
+						}
+					}
+				}
+			}
+			
+			else if (arg.equals(contentNameArg)) {
+				if ((index+1) < (args.length)) {
+					if (args[index + 1].startsWith("--")) {
+						System.out.println("incorrect input");
+						break;
+					}
+					else
+						commands.put(contentNameArg, args[index+1]);
+				}
+				
+				else 
+					System.out.println("incorrect input");
+			}
+				
+			
+			else if (arg.equals(contentContextArg)) {
+				if ((index+1) < (args.length)) {
+					if (args[index + 1].startsWith("--")) {
+						System.out.println("incorrect input");
+						break;
+					}
+					else
+						commands.put(contentContextArg, args[index+1]);
+				}
+				else 
+					System.out.println("incorrect input");
+			}
+				
+			index++;
+		}
+		
+		/*for (String hash : commands.keySet())
+		{
+			System.out.println (hash +" "+ commands.get(hash));
+		} */
+		
+		// System.out.println (validateCommand());
+	}
+}
